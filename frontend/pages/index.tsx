4cea173aaf1fa06f34c05e7b11621cfd24402d20
theof <jufo.theo@gmail.com>
2022-02-13 11:37:10 +0100

    frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,12 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import type { NextPage } from 'next'
+import { Typography } from '@mui/material'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    
+    <Typography variant="h1">hi</Typography>
   )
 }
 
